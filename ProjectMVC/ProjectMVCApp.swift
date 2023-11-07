eb6bb663a7824e6f40a3a3cff0151d9869da2b07
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-11-07 19:37:05 -0300

    Fazer o commit inicial

---
+++ ProjectMVC/ProjectMVCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectMVCApp.swift
+//  ProjectMVC
+//
+//  Created by Rafael Gonzaga on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectMVCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
