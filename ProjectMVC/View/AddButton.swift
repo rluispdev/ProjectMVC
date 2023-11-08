aec1a46800be4f461ce48d2fdc352c2bdd4fb456
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-11-08 19:37:55 -0300

    Adicionar a View AddButton

---
+++ ProjectMVC/View/AddButton.swift
@@ -0,0 +1,18 @@
+//
+//  AddButton.swift
+//  ProjectMVC
+//
+//  Created by Rafael Gonzaga on 08/11/23.
+//
+
+import SwiftUI
+
+struct AddButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddButton()
+}
