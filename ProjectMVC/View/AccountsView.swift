1b6259ff03d9567e7d57007bf5e1432a3582a20f
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-11-08 20:11:33 -0300

    Criar conteúdo

---
+++ ProjectMVC/View/AccountsView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountsView.swift
+//  ProjectMVC
+//
+//  Created by Rafael Gonzaga on 08/11/23.
+//
+
+import SwiftUI
+
+struct AccountsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountsView()
+}
